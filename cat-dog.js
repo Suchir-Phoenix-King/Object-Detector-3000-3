9ed06887e37abc4b2bd2f203d7f4afb8ad8e02e5
Suchir-Phoenix-King <88494177+Suchir-Phoenix-King@users.noreply.github.com>
2022-06-20 18:00:46 +0530

    Add files via upload

---
+++ cat-dog.js
@@ -0,0 +1,41 @@
+status = "";
+img = "";
+objects = [];
+
+function preload() {
+    img = loadImage("cat-dog.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    document.getElementById("num_of_obj_cat-dog").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.log("Model loaded!!!")
+    status = true;
+    objectDetector.detect(video, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    if(status != "") {
+        for(i = 0; i < objects.length; i++) {
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("num_of_obj_bed").innerHTML = "There are 2 big images in which cocossd has detected 0";
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,28 @@
+function back() {
+    window.location.href = "index.html";
+}
+
+function goto1() {
+    window.location.href = "Bed.html";
+}
+
+function goto2() {
+    window.location.href = "TV.html";
+}
+
+function goto3() {
+    window.location.href = "Sofa.html";
+}
+
+function goto4() {
+    window.location.href = "cat-dog.html";
+}
+
+function goto5() {
+    window.location.href = "Dining Table.html";
+}
+
+function goto6() {
+    window.location.href = "Grand-Piano.html";
+}
+
